ef830a128b55544f7efc66ebd474b1f096ac50e0
Dmitri Safine <dsafine@tales.social>
2024-01-02 10:47:08 -0800

    key maps and refreshing tree on git status update

--- nvim/lua/dsafine/plugins/neogit.lua
+++ nvim/lua/dsafine/plugins/neogit.lua
@@ -4,6 +4,21 @@ return {
 		"nvim-lua/plenary.nvim",
 		"sindrets/diffview.nvim",
 		"nvim-telescope/telescope.nvim",
+		"nvim-tree/nvim-tree.lua",
 	},
-	config = true,
+	config = function()
+		-- configure setup
+		local neogit = require("neogit")
+		neogit.setup({
+			kind = "split",
+		})
+
+		-- set nvim-tree to refresh on git status update
+		vim.cmd([[ autocmd User NeogitStatusRefreshed :NvimTreeRefresh ]])
+
+		-- configure key maps
+		local keymap = vim.keymap
+		keymap.set("n", "<leader>gg", "<cmd>Neogit<CR>", { desc = "Open git" })
+		keymap.set("n", "<leader>gc", "<cmd>Neogit commit<CR>", { desc = "Open git commit popup" })
+	end,
 }
