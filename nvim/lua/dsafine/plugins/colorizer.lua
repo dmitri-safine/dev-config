700416c5acffb7fa3fc07d1a557b7f079e0a7dd3
Dmitri Safine <dsafine@tales.social>
2024-01-03 18:14:40 -0800

    making colorizer highlight only css and html files

--- nvim/lua/dsafine/plugins/colorizer.lua
+++ nvim/lua/dsafine/plugins/colorizer.lua
@@ -1,5 +1,10 @@
 return {
-  "NvChad/nvim-colorizer.lua",
-  event = { "BufReadPre", "BufNewFile" },
-  config = true,
+	"NvChad/nvim-colorizer.lua",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local colorizer = require("colorizer")
+		colorizer.setup({
+			filetypes = { "css", "html" },
+		})
+	end,
 }
