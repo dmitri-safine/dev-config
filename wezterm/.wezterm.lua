8a725b6447cf56abc42c33e70a443615b437ba81
Dmitri Safine <dsafine@ontomap.com>
2024-06-15 14:41:21 -0500

    wezterm pane switching

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,5 +1,6 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
+local act = wezterm.action
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -25,6 +26,21 @@ if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 end
 
 config.launch_menu = launch_menu
+
+-- configure key shortcuts
+local keys = {}
+
+-- pane switching
+-- activate pane selection mode with
+-- the default alphabet
+-- (labels are "a", "s", "d", "f" and so on)
+table.insert(keys, {
+	key = "8",
+	mods = "CTRL",
+	action = act.PaneSelect,
+})
+
+config.keys = keys
 
 -- and finally, return the configuration to wezterm
 return config
