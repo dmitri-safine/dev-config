6e6214d38168fb6127cc511366f9680c11179a4a
Safine <dmitri.safine@chevron.com>
2024-08-01 13:44:45 -0500

    configure nvim-dap ui

--- nvim/lua/dsafine/plugins/nvim-dap.lua
+++ nvim/lua/dsafine/plugins/nvim-dap.lua
@@ -14,9 +14,30 @@ return {
 				name = "launch - netcoredbg",
 				request = "launch",
 				program = function()
-					return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
+					return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "\\bin\\Debug\\net8.0\\", "file")
 				end,
 			},
 		}
+
+		local keymap = vim.keymap
+		keymap.set("n", "<F5>", "<cmd>lua require'dap'.continue()<CR>", { desc = "Continue" })
+		keymap.set("n", "<F10>", "<cmd>lua require'dap'.step_over()<CR>", { desc = "Step over" })
+		keymap.set("n", "<F11>", "<cmd>lua require'dap'.step_into()<CR>", { desc = "Step into" })
+		keymap.set("n", "<F12>", "<cmd>lua require'dap'.step_out()<CR>", { desc = "Step out" })
+		keymap.set("n", "<leader>b", "<cmd>lua require'dap'.toggle_breakpoint()<CR>", { desc = "Toggle breakpoint" })
+		keymap.set(
+			"n",
+			"<leader>qc",
+			"<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
+			{ desc = "Set conditional breakpoint" }
+		)
+		keymap.set(
+			"n",
+			"<leader>qm",
+			"<cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>",
+			{ desc = "Set breakpoint with message" }
+		)
+		keymap.set("n", "<leader>qr", "<cmd>lua require'dap'.repl.open()<CR>", { desc = "Open debugger REPL" })
+		keymap.set("n", "<leader>ql", "<cmd>lua require'dap'.run_last()<CR>", { desc = "Run last with the debugger" })
 	end,
 }
