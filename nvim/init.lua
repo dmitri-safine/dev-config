a4305db3edd53472c5b15283c5265748d33a4dc0
Dmitri Safine <dsafine@ontomap.com>
2024-11-03 11:06:03 -0600

    change python executable path

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,5 +4,4 @@ require("dsafine.lazy")
 -- Provider configs
 
 -- Python
-vim.g.python_host_prog = "/usr/bin/python3"
-vim.g.python3_host_prog = "/usr/bin/python3"
+vim.g.python3_host_prog = vim.fn.expand("$HOME") .. "\\scoop\\apps\\python\\current\\python.exe"
