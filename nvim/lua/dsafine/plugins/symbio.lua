0dd3d69bcee66bc9b10d1fd30a6caffaa3514612
Dmitri Safine <dsafine@ontomap.com>
2025-11-02 21:01:43 -0600

    add symbio plugin

---
+++ nvim/lua/dsafine/plugins/symbio.lua
@@ -0,0 +1,18 @@
+return {
+	"ontomap/symbio-nvim",
+	lazy = false, -- load at startup so the watcher is active
+	config = function()
+		require("symbio").setup({
+			vault_dir = vim.fn.expand("g:/My Drive/obsidian/vaults/symbio"), -- root of your vault
+			inbox_dir = nil, -- defaults to vault_dir .. "/inbox"
+			archive_dir = nil, -- defaults to vault_dir .. "/.archives"
+			retention = 0, -- keep archives indefinitely, change tokeep last N archives
+			highlight_ms = 2000, -- highlight duration for new files
+			watcher = {
+				enabled = true,
+				debounce_ms = 200,
+			},
+		})
+	end,
+	dependencies = { "nvim-tree/nvim-tree.lua" },
+}
