03e2f96ea4edb30490f68d20f2b207a442191800
Dmitri Safine <dsafine@ontomap.com>
2025-11-03 14:09:16 -0600

    change inbox archive folder

--- nvim/lua/dsafine/plugins/symbio.lua
+++ nvim/lua/dsafine/plugins/symbio.lua
@@ -5,7 +5,7 @@ return {
 		require("symbio").setup({
 			vault_dir = vim.fn.expand("g:/My Drive/obsidian/vaults/symbio"), -- root of your vault
 			inbox_dir = nil, -- defaults to vault_dir .. "/inbox"
-			archive_dir = nil, -- defaults to vault_dir .. "/.archives"
+			archive_dir = vim.fn.expand("g:/My Drive/obsidian/vaults/symbio/backups/inbox-archives"), -- defaults to vault_dir .. "/.archives"
 			retention = 0, -- keep archives indefinitely, change tokeep last N archives
 			highlight_ms = 2000, -- highlight duration for new files
 			watcher = {
